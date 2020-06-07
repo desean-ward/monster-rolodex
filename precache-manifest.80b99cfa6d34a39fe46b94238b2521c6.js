5e197ddc7e0a027f815ec3f0f70a2fcd2c040ac2
web-development <dward1711@gmail.com>
2020-06-07 08:45:18 -0500

    Updates

---
+++ precache-manifest.80b99cfa6d34a39fe46b94238b2521c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01d2fbe67ee5341d04c93d1cdf931ae",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d20ea04eeb0b3b3afe67",
+    "url": "/monster-rolodex/static/css/main.810841a3.chunk.css"
+  },
+  {
+    "revision": "d681964e80211e0f86b5",
+    "url": "/monster-rolodex/static/js/2.615db946.chunk.js"
+  },
+  {
+    "revision": "81c1ab2ba64ac9f85d481dd74a981dc6",
+    "url": "/monster-rolodex/static/js/2.615db946.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d20ea04eeb0b3b3afe67",
+    "url": "/monster-rolodex/static/js/main.a9f5919e.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
